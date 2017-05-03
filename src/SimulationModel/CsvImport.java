970e1a1dc8e699dc5393931b4898e19c20408905
S-Docker <steven.docker@hotmail.co.uk>
2017-05-03 12:54:03 +0100

    Javadoc added to CsvImport class

--- src/SimulationModel/CsvImport.java
+++ src/SimulationModel/CsvImport.java
@@ -7,7 +7,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * Created by Steven on 30/04/2017.
+ * This class takes two filename.csv Strings as parameters and processes the information found within them.
+ * one .csv file containing the traded companies, their stock type, the number of shares issued and the value of the
+ * stock, the other .csv file contains the names of each client, their cash holdings, and the shares they hold in each
+ * company.
+ *
+ * It then processes this information using BufferedReaders to create two ArrayLists containing the TradedCompany and
+ * Portfolio objects to be passed to the TradingExchange.
+ * @version 1.0
+ * @author SDocker
  */
 public class CsvImport {
 
@@ -28,6 +36,14 @@ public class CsvImport {
      *                  Constructors
      *
      ****************************************************/
+    /**
+     * Set tradedCompanyBR and portfolioBR Buffer readers to the .csv string path provided in the constructor param,
+     * run the corresponding methods for processing the .csvs
+     *
+     *
+     * @param companyCSV String of the Traded Company .csv file path, portfolioCSV String of the Portfolio .csv file path
+     * @Exception e throw exception is the .csv file cannot be read
+     */
     public CsvImport(String companyCSV, String portfolioCSV) throws FileNotFoundException {
         this.tradedCompanyBR = new BufferedReader(new FileReader(new File(companyCSV)));
         this.portfolioBR = new BufferedReader(new FileReader(new File(portfolioCSV)));
@@ -50,6 +66,12 @@ public class CsvImport {
         }
     }
 
+    /**
+     * For each line within the BufferedReader, split the line by ',' into tradedCompanyEntries String array,
+     * Use string elements within String array to create new TradedCompany and put into tradedCompanies HashMap
+     *
+     * @param companies BufferedReader object pointing to Traded Companies .csv file
+     */
     public void readDataTradedCompanies(BufferedReader companies) throws IOException {
         while ((line = companies.readLine()) != null) {
             String[] tradedCompanyEntries = line.split(",");
@@ -57,10 +79,20 @@ public class CsvImport {
         }
     }
 
+    /**
+     * Returns whether the traded company key exists within tradedCompanies HashMap
+     *
+     * @return a boolean, true if the key exists within tradedCompanies HashMap
+     */
     public boolean tradedCompanyExists(String key){
         return tradedCompanies.containsKey(key);
     }
 
+    /**
+     * Return the traded company object matching the String key within HashMap
+     *
+     * @return the HashMap value corresponding to company name or null if key does not exist
+     */
     public TradedCompany getTradedCompany(String key){
         if (tradedCompanyExists(key)){
             return tradedCompanies.get(key);
@@ -68,6 +100,14 @@ public class CsvImport {
         return null;
     }
 
+    /**
+     * For each line within the BufferedReader, split the line by ',' into portfolioEntries String array and add to
+     * ArrayList of strings.
+     * Get first element of ArrayList containing all company names and iterate through remaining elements to obtain
+     * portfolio information and create/add Portfolio objects to Portfolio HashMap
+     *
+     * @param portfolios BufferedReader object pointing to Portfolio .csv file
+     */
     public void readDataPortfolio(BufferedReader portfolios) throws IOException {
         while ((line = portfolios.readLine()) != null) {
             String[] portfolioEntries = line.split(",");
