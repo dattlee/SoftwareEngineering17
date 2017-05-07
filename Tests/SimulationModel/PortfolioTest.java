1c43ea5048f2bd3f74eb2c72685b8048fbec37b1
Dattlee <attleed@gmail.com>
2017-05-07 13:19:06 +0100

    Portfolio Tests

--- Tests/SimulationModel/PortfolioTest.java
+++ Tests/SimulationModel/PortfolioTest.java
@@ -1,8 +1,11 @@
 package SimulationModel;
 
 
+import dattlee.usefuls.Pair;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -12,23 +15,27 @@ import static org.junit.Assert.assertTrue;
  */
 public class PortfolioTest {
 
-    Portfolio port1;
+    Portfolio port1,port2;
+    TradedCompany google;
 
     @Before
     public void setUp() throws Exception {
-        port1 = new Portfolio("1001"); // Should initialize Risk to low by default
+        port1 = new Portfolio("Megan"); // Should initialize Risk to low by default
+        google = new TradedCompany("google",ShareType.HITECH,24494,50);
+        Pair<TradedCompany,Integer>[] shares = new Pair[]{new Pair<>(google,14)};
+        port2 = new Portfolio("temp",shares,1234.56);
     }
 
     @Test
     public void getId() throws Exception {
-        assertEquals(port1.getName(),"1001");
+        assertEquals(port1.getName(),"Megan");
     }
 
     @Test
     public void setId() throws Exception {
-        assertEquals(port1.getName(),"1001");
-        port1.setName("2001");
-        assertEquals(port1.getName(),"2001");
+        assertEquals(port1.getName(),"Megan");
+        port1.setName("Charlie");
+        assertEquals(port1.getName(),"Charlie");
     }
 
     @Test
@@ -49,72 +56,95 @@ public class PortfolioTest {
     @Test
     public void getSharesHashMap() throws Exception {
         // need to assert that the hashmap returned contains all companies and shares added to it
-        port1.getTotalSharesValue();
-        port1.getShares();
 
+
+        assertEquals(port1.getShares().entrySet().size(),0);
+        port1.buyShares(google,12);
+
+        assertEquals(port1.getShares().entrySet().size(),1);
+        for(HashMap.Entry<TradedCompany,Integer> ent :port1.getShares().entrySet()) {
+            assertTrue(ent.getKey().equals(google));
+        }
     }
 
     @Test
     public void getSharesSinglecompany() throws Exception {
         // Need to assert a singe number/ Integer is returned
-        assertTrue(1==0);
+
+        assertTrue(port2.getShares(google).equals(14));
+
     }
 
+    /**
+     * Need to assert:
+     * 1. When buying shares you add the number of shares bought to the
+     */
     @Test
     public void buyShares() throws Exception {
-        /*
-        Need to assert:
-        1. When buying shares you add the number of shares bought to the
-        */
-        assertTrue(1==0);
+
+        assertEquals(port1.getShares().entrySet().size(),0);
+        port1.buyShares(google,12);
+        assertTrue(port1.getShares(google).equals(12));
     }
 
+    /**
+     * Asserts
+     * 1. Can't sell more shares than owned for a company
+     * 2. Can't sell shares for a company not owned
+     */
     @Test
     public void sellShares() throws Exception {
-        /*
-        Need to assert:
-        1. Can't sell more shares than owned for a company
-        2. Can't sell shares for a company not owned
-         */
-        assertTrue(1==0);
+
+        assertEquals(port2.getShares().entrySet().size(),1);
+        port2.sellShares(google,14);
+        assertTrue(port2.getShares(google).equals(0));
+        assertTrue(port1.getShares(google).equals(0));
     }
 
+    /**
+     * Need to assert:
+     * 1. Can't sell more shares than owned for a company
+     * 2. Can't sell shares for a company not owned
+     */
     @Test
     public void getCash() throws Exception {
-        /*
-        Need to assert:
-        1. Can't sell more shares than owned for a company
-        2. Can't sell shares for a company not owned
-         */
-        assertTrue(1==0);
+
+        assertTrue(port2.getCash() == 1234.56);
     }
 
+    /**
+     * Asserts:
+     * 1. Cash total increases by exactly amount given
+     */
     @Test
     public void addCash() throws Exception {
-        /*
-        Need to assert:
-        1. Cash total increases by exactly amount given
-         */
-        assertTrue(1==0);
+        assertTrue(port2.getCash() == 1234.56);
+        port2.addCash(1000);
+        assertTrue(port2.getCash() == 2234.56);
+
     }
 
+    /**
+     * Need to assert:
+     * 1. Cash total decreases by exactly taken given
+     */
     @Test
     public void removeCash() throws Exception {
-        /*
-        Need to assert:
-        1. Cash total decreases by exactly taken given
-         */
-        assertTrue(1==0);
+        assertTrue(port2.getCash() == 1234.56);
+        port2.removeCash(1);
+        assertTrue(port2.getCash() == 1233.56);
     }
 
+    /**
+     * Assert:
+     * 1. Cash is added to value of portfolio
+     * 2. Shares are added to the value of the portfolio
+     */
     @Test
     public void getValue() throws Exception {
-        /*
-        Need to assert:
-        1. Cash is added to value of portfolio
-        2. Shares are added to the value of the portfolio
-         */
-        assertTrue(1==0);
+        System.out.println(port2.getValue());
+        assertTrue(port1.getValue() == 0);
+        assertTrue(port2.getValue() == 1934.56);
     }
 
 
